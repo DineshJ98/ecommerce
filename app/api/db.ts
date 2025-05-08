422d8ef4103a3612fd3eae016b4f17f6ea4d3f45
AzureAD\DineshJeewantha <jeew@sitecore.net>
2025-05-08 20:04:15 +0530

    connect db to product api

--- app/api/db.ts
+++ app/api/db.ts
@@ -1,22 +1,31 @@
-import { MongoClient, ServerApiVersion } from "mongodb";
+import { Db, MongoClient, ServerApiVersion } from "mongodb";
+
+let cachedClient : MongoClient | null  = null;
+let cachedDb : Db | null = null;
 
 export async function connectToDB(){
-    const uri = `mongodb+srv://${process.env.MONGODB_USER}:${process.env.MONGODB_PASSWORD}@cluster0.fzbsrld.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
-    // Create a MongoClient with a MongoClientOptions object to set the Stable API version
-    const client = new MongoClient(uri, {
-      serverApi: {
-        version: ServerApiVersion.v1,
-        strict: true,
-        deprecationErrors: true,
-      }
-    });
-    
-    try {
-        await client.connect();
-    } finally {
-        await client.close();
+  if ( cachedClient && cachedDb){
+    return { client : cachedClient , db : cachedDb}
+  }
+
+  const uri = `mongodb+srv://${process.env.MONGODB_USER}:${process.env.MONGODB_PASSWORD}@cluster0.fzbsrld.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+
+  // Create a MongoClient with a MongoClientOptions object to set the Stable API version
+  const client = new MongoClient(uri, {
+    serverApi: {
+      version: ServerApiVersion.v1,
+      strict: true,
+      deprecationErrors: true,
     }
-    
+  });
+  
+
+  await client.connect();
+  
+  cachedClient = client;
+  cachedDb = client.db('ecommerce-nextjs');
+
+  return { client, db : client.db()}  
 }
 
