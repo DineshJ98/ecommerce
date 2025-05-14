bb9d3f2c4743efa9c8a513707c61817dfb6d792f
AzureAD\DineshJeewantha <jeew@sitecore.net>
2025-05-14 20:26:40 +0530

    add styles to products page

--- app/ProductsList.tsx
+++ app/ProductsList.tsx
@@ -4,12 +4,19 @@ import Link from "next/link";
 
 export default function ProductsList({product}:{product: Product[]}){
     return(
-        <div>
+        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-8">
             {product.map(product => (
-                <Link key={product.id} href={'/products/' + product.id}>
-                    <Image src={'/' + product.imageURL} alt={product.name} width={150} height={150}/>
-                    <h3>{product.name}</h3>
-                    <b>{'$'+ product.price}</b>
+                <Link 
+                key={product.id} 
+                href={'/products/' + product.id}
+                className="bg-white text-black rounded-lg shadow-md p-4 hover:shadow-lg transition duration-300"
+                >
+                    <div className="flex justify-center mb-4 h-48 relative">
+                        <Image src={'/' + product.imageURL} alt={product.name} width={150} height={150} className="object-cover ronded-md"/>
+                    </div>
+                    <h2 className="text-xl font-semibold mb-2">{product.name}</h2>
+                    <p className="text-gray-600">{'$'+ product.price}</p>
+                    
                 </Link>
             ))}
         </div>
