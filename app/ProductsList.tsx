10c9f440d82b084a35bc9bff042fb2bf345ed711
AzureAD\DineshJeewantha <jeew@sitecore.net>
2025-05-20 21:30:42 +0530

    create delete function in the product page

--- app/ProductsList.tsx
+++ app/ProductsList.tsx
@@ -24,10 +24,29 @@ export default function ProductsList({product , initialCartProducts = []}:{produ
         setCartProducts(updatedCart);
     }
 
+    function itemIsInCart( itemId : string) {
+        return cartProcuts.some( cp => cp.id === itemId); 
+    }
+
+    async function removeFromCart(productId : string) {
+        const response = await fetch('http://172.18.73.80:3000/api/users/2/cart', {
+            method: 'DELETE',
+            body: JSON.stringify({
+                productId,
+            }),
+            headers: {
+                'Content-Type': 'application/json',
+            }
+        });
+
+        const updatedCart = await response.json();
+        setCartProducts(updatedCart);
+    }
     
     return(
         <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-8">
             {product.map(product => (
+
                 <Link 
                 key={product.id} 
                 href={'/products/' + product.id}
@@ -38,8 +57,30 @@ export default function ProductsList({product , initialCartProducts = []}:{produ
                     </div>
                     <h2 className="text-xl font-semibold mb-2">{product.name}</h2>
                     <p className="text-gray-600">{'$'+ product.price}</p>
-                    <button onClick={() => addToCart(product.id)}>Add to cart</button>
+                    {
+                        itemIsInCart(product.id) ?
+                            <button
+                                className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-2 rounded w-full" 
+                                onClick={(e) => {
+                                e.preventDefault();
+                                removeFromCart(product.id);
+                                }}>
+                                Remove from the cart
+                            </button>
+                        :
+                            <button 
+                                className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-2 rounded w-full" 
+                                onClick={(e) => {
+                                e.preventDefault();
+                                addToCart(product.id);
+                                }}>
+                           Add to Cart 
+                        </button>
+
+                    }
+                    
                 </Link>
+
             ))}
         </div>
     )
