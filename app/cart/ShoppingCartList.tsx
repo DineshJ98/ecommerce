adc3b7f800f9206aeacddafc5963c4ce9a942fbb
AzureAD\DineshJeewantha <jeew@sitecore.net>
2025-05-21 21:13:47 +0530

    create remove funtionality in cart

--- app/cart/ShoppingCartList.tsx
+++ app/cart/ShoppingCartList.tsx
@@ -4,7 +4,22 @@ import { useState } from "react";
 import { Product } from "../product-data";
 
 export default function ShoppingCartList( { initialCartProducts } : { initialCartProducts : Product[]} ){
-    const [cartProducts] = useState(initialCartProducts);
+    const [cartProducts, setCartProducts] = useState(initialCartProducts);
+
+    async function removeProduct(productId : string){
+        const response = await fetch('http://172.18.73.80:3000/api/users/2/cart', {
+            method: 'DELETE',
+            body: JSON.stringify({
+                productId,
+            }),
+            headers: {
+                'Content-Type': 'application/json'
+            },
+        });
+
+        const updatedCart = await response.json(); 
+        setCartProducts(updatedCart);
+    }
 
     return (
     <div className="container mx-auto p-8">
@@ -16,6 +31,15 @@ export default function ShoppingCartList( { initialCartProducts } : { initialCar
                 <Link href={ '/products/' + product.id }>
                     <h3 className="text-xl text-black font-semibold mb-2">{product.name}</h3>
                     <p className="text-gray-600">${product.price}</p>
+                    <button
+                        className="bg-blue-500 text-white font-bold px-2 py-2 rounded hover:bg-blue-700"
+                        onClick={
+                            (e) => {
+                                e.preventDefault();
+                                removeProduct(product.id);
+                            }
+                        }                       
+                    >Remove From Cart</button>
                 </Link>
                 </li>
             ))
